a62fb6486e4ce1213248f34675d8d48e241c359a
BudLight-Year <totallysweethobo@gmail.com>
2024-11-16 23:24:27 -0700

    working but broken model implementation. commiting before ruining everything

--- MLWebApp/recommendations/urls.py
+++ MLWebApp/recommendations/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, reverse_lazy
-from . import views
+from .views import RecommendationDetailView, RecommendationEngineView
 
 app_name = "recommendations"
 urlpatterns = [
     #path("", views.index, name="index"),
+    path("engine", RecommendationEngineView.as_view(), name='engine'),
+    path('<int:recommendation_id>/', RecommendationDetailView.as_view(), name='recommendation_detail'),
 ]
