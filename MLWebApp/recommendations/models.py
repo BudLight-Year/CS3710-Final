86f3a2b38524c7c1b7d360acc2681931d1c9850d
BudLight-Year <totallysweethobo@gmail.com>
2024-11-16 21:51:26 -0700

    added ML model and load function in view.

--- MLWebApp/recommendations/models.py
+++ MLWebApp/recommendations/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import tensorflow as tf
+from keras.saving import register_keras_serializable
 
 # Create your models here.
 
@@ -13,6 +15,7 @@ class Movie(models.Model):
 
 class Recommendation(models.Model):
     user = models.ForeignKey('user.Myuser', on_delete=models.DO_NOTHING, related_name="recommendations")
+    preference = models.ForeignKey('recommendations.Preference', on_delete=models.CASCADE, related_name="recommendation")
     movies = models.ManyToManyField(Movie, related_name="recommendations")
 
     def __str__(self): 
@@ -20,7 +23,6 @@ class Recommendation(models.Model):
 
 
 class Preference(models.Model):
-    recommendation = models.ForeignKey('recommendations.Recommendation', on_delete=models.CASCADE, related_name="recommendation")
     genre1 = models.CharField(max_length=255)
     genre2 = models.CharField(max_length=255, blank=True, null=True)
     genre3 = models.CharField(max_length=255, blank=True, null=True)
@@ -33,4 +35,58 @@ class Preference(models.Model):
 # Do this one last
 class SimilarMovie(models.Model):
     pass
+
+
+#------------------------#
+#                        #
+#      ML MODEL          #
+#                        #
+#------------------------#
+
+
+@register_keras_serializable()
+class MovieGenreModel(tf.keras.Model):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        # Initialize layers with None - they will be properly configured when the model is loaded
+        self.movie_embedding = None
+        self.genre_embedding = None
+        self.rating_predictor = None
+        
+    def build(self, input_shape):
+        # This method is called when the model is being built/loaded
+        if self.movie_embedding is None:
+            self.movie_embedding = tf.keras.Sequential([
+                tf.keras.layers.StringLookup(mask_token=None),
+                tf.keras.layers.Embedding(
+                    input_dim=1000,  # This will be configured from saved weights
+                    output_dim=32
+                )
+            ])
+            
+        if self.genre_embedding is None:
+            self.genre_embedding = tf.keras.Sequential([
+                tf.keras.layers.Dense(64, activation='relu'),
+                tf.keras.layers.Dense(32)
+            ])
+            
+        if self.rating_predictor is None:
+            self.rating_predictor = tf.keras.Sequential([
+                tf.keras.layers.Dense(32, activation='relu'),
+                tf.keras.layers.Dense(16, activation='relu'),
+                tf.keras.layers.Dense(1)
+            ])
+        
+        super().build(input_shape)
+
+    def call(self, inputs):
+        movie_emb = self.movie_embedding(inputs['movieId'])
+        genre_emb = self.genre_embedding(inputs['genre_preferences'])
+        combined = tf.concat([movie_emb, genre_emb], axis=1)
+        return self.rating_predictor(combined)
+
+    def get_config(self):
+        config = super().get_config()
+        return config
 
--- MLWebApp/recommendations/views.py
+++ MLWebApp/recommendations/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views import View
+from .forms import PreferenceForm
+from keras.models import load_model
+import tensorflow as tf
+import numpy as np
+import os
+from .models import MovieGenreModel  # Import the model class we just created
 
-# Create your views here.
+# Load the Keras model (relative path) 
+model_path = os.path.join(os.path.dirname(__file__), 'genreModel.keras')
+
+try:
+    # Load the model with compile=False first
+    model = load_model(
+        model_path, 
+        custom_objects={'MovieGenreModel': MovieGenreModel}, 
+        compile=False
+    )
+    
+    # Then compile it after loading
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(0.001),
+        loss=tf.keras.losses.MeanSquaredError()
+    )
+except Exception as e:
+    print(f"Error loading model: {e}")
+    raise
+
+
+GENRE_CHOICES = [
+    'Action', 'Adventure', 'Animation', 'Children', 'Comedy',
+    'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir',
+    'Horror', 'IMAX', 'Musical', 'Mystery', 'Romance', 'Sci-Fi',
+    'Thriller', 'War', 'Western'
+]
+
+class CreatePreferenceView(View):
+    def get(self, request):
+        form = PreferenceForm()
+        return render(request, 'create_preference.html', {'form': form})
+
+    def post(self, request):
+        form = PreferenceForm(request.POST)
+        if form.is_valid():
+            preference = form.save(commit=False)
+            genre_preferences = self.prepare_genre_preferences(preference)
+            
+            # Create the input structure the model expects
+            model_input = {
+                'movieId': tf.constant(['1']),  # Example movieId, adjust as needed
+                'genre_preferences': tf.constant([genre_preferences], dtype=tf.float32)
+            }
+            
+            predictions = model.predict(model_input)
+            return HttpResponse(f'Model Predictions: {predictions}')
+        return render(request, 'create_preference.html', {'form': form})
+
+    def prepare_genre_preferences(self, preference):
+        genre_preferences = np.zeros(len(GENRE_CHOICES))
+        selected_genres = [
+            preference.genre1, preference.genre2, preference.genre3,
+            preference.genre4, preference.genre5
+        ]
+        
+        for i, genre in enumerate(GENRE_CHOICES):
+            if genre in selected_genres:
+                genre_preferences[i] = 1
+            elif preference.include_other_genres:
+                genre_preferences[i] = 0
+            else:
+                genre_preferences[i] = -1
+                
+        return genre_preferences
