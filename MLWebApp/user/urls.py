9229baff1db0984cd8927345a557184151dd793f
BudLight-Year <totallysweethobo@gmail.com>
2024-11-20 15:40:45 -0700

    need to fix unit tests.

--- MLWebApp/user/urls.py
+++ MLWebApp/user/urls.py
@@ -20,7 +20,6 @@ urlpatterns = [
     path('confirm-email/<key>/', ConfirmEmailView.as_view(), name='account_confirm_email'),
     path("update-account/", views.update_account, name='update_account'),
     path("update-profile/", views.update_profile, name='update_profile'),
-    path("change-password/", views.change_password, name='change_password'),
     path('password-reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('user:password_reset_done')), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('user:password_reset_complete')), name='password_reset_confirm'),
