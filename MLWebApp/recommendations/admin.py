eec4ec2376be859a4a6d05bef17d80ea66db7a4d
BudLight-Year <totallysweethobo@gmail.com>
2024-11-26 19:01:41 -0700

    registered models with admin site

--- MLWebApp/recommendations/admin.py
+++ MLWebApp/recommendations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Preference, Recommendation, Movie, Feedback
 
 # Register your models here.
+admin.site.register(Preference)
+admin.site.register(Recommendation)
+admin.site.register(Movie)
+admin.site.register(Feedback)
